1242150a6dfa1bb811b85010eb4dfd9dbe276ce8
Emily Eisenberg <xymostech@gmail.com>
2015-01-23 23:31:55 -0800

    Make the scheduler a bit better

--- monitor.py
+++ monitor.py
@@ -22,18 +22,13 @@ schedule = [
 TEMP_THRESHOLD_LOW = 21.0
 TEMP_THRESHOLD_HIGH = 21.5
 
-class Schedule:
-    def __init__(self, heat_event):
+class Schedule(object):
+    def __init__(self):
         self.heat_on = False
-        self.heat_event = heat_event
-
-        self.heat_event.clear()
-
         gpio.heat_off()
 
     def run(self):
         now = datetime.datetime.now()
-
         hour = now.hour
 
         temp = gpio.get_temp()
@@ -41,25 +36,20 @@ class Schedule:
         if self.heat_on and (not schedule[hour] or temp > TEMP_THRESHOLD_HIGH):
             gpio.heat_off()
             self.heat_on = False
-            self.heat_event.clear()
         elif not self.heat_on and schedule[hour] and temp < TEMP_THRESHOLD_LOW:
             gpio.heat_on()
             self.heat_on = True
-            self.heat_event.set()
 
 
-def monitor_func(stop_event, heat_event):
-    schedule = Schedule(heat_event)
-    schedule.run()
-
+def monitor_func(stop_event, scheduler):
     while not stop_event.is_set():
-        schedule.run()
+        scheduler.run()
 
         time.sleep(30)
 
 monitor_thread = None
 stop_event = threading.Event()
-heat_event = threading.Event()
+scheduler = Schedule()
 
 def start_monitoring():
     global monitor_thread
@@ -67,7 +57,7 @@ def start_monitoring():
         stop_event.clear()
         monitor_thread = threading.Thread(
             target=monitor_func,
-            args=(stop_event, heat_event)
+            args=(stop_event, scheduler)
         )
         monitor_thread.start()
 
