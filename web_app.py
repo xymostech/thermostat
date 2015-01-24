6c54a09bec2a8c365b9da1364e521f333dfeff0b
Emily Eisenberg <xymostech@gmail.com>
2015-01-23 23:44:24 -0800

    Add some API endpoints

--- web_app.py
+++ web_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 import gpio
@@ -12,6 +12,16 @@ def main():
     }
 
     return render_template('index.html', **context)
+
+
+@app.route('/api/heater_on')
+def heater_on():
+    return jsonify(heat=monitor.heat_on())
+
+
+@app.route('/api/current_temp')
+def current_temp():
+    return jsonify(temp=gpio.get_temp())
 
 
 if __name__ == "__main__":
